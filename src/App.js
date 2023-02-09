a97088d1101fe73cf8b8bf195cbdb3ae4caa9c11
Ryum <ryumforart@gmail.com>
2023-02-09 10:37:26 +0900

    assignment implement complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const counter = useRef(1);
+
+  const [obj, setObj] = useState([
+    {
+      id : 0,
+      title : 'something in here'
+    }
+  ]);
+
+  const [ipt, setIpt] = useState('');
+
   return (
-    <div>
+    <div className = 'container'>
+      <div>
+        <input value = {ipt} onChange = {e => setIpt(e.target.value)}/>
+        <button onClick = {() => {
+          const tmpObj = {
+            id : counter.current,
+            title : 'ipt'
+          }
+          setObj(obj.concat(tmpObj));
+          counter.current += 1;
+        }}>추가하기</button>
+      </div>
+      <h1>Todo List</h1>
+      <div className = 'objContainer'>
+        {obj.map(v => <Contents className = 'Contents' key = {v.id} obj = {v}>{v}</Contents>)}
+      </div>
     </div>
   );
 }
+
+const Contents = ({ obj }) => {
+  return (
+    <div className = 'contents'>{obj.title}</div>
+  );
+};
 
 export default App;
